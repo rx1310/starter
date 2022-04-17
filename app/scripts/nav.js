52def925ded09bcc2a2276aba80b6bcb77c4c728
rx1310 <rx1310@inbox.ru>
2022-04-17 13:53:13 +0300

    test: добавляет скрипт для Nav

---
+++ app/scripts/nav.js
@@ -0,0 +1,23 @@
+const nav               = document.querySelector('.nav');
+const navTitle          = document.querySelector('.nav__title');
+const navScrollUp       = document.getElementById("scrollUp");
+const navScrolledClass  = "nav--scrolled";
+const navActionDisabled = "nav__action--disabled";
+
+window.addEventListener('scroll', (e) => {
+
+	if (window.pageYOffset > 150) {
+
+		nav.classList.add(navScrolledClass);
+		navTitle.classList.remove('hidden');
+		navScrollUp.classList.remove(navActionDisabled);
+
+	} else {
+
+		nav.classList.remove(navScrolledClass);
+		navTitle.classList.add('hidden');
+		navScrollUp.classList.add(navActionDisabled);
+
+	}
+
+});
